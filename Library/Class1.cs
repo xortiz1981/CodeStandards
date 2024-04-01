3ce4e7a03fb9462dbdbe12de1ce603e339dc2152
xortiz1981 <108493831+xortiz1981@users.noreply.github.com>
2024-04-01 12:04:35 -0500

    created a super basic test to use commands written in README

--- Library/Class1.cs
+++ Library/Class1.cs
@@ -2,5 +2,7 @@
 
 public class Class1
 {
-
+    public static int demo(){
+        return 14;
+    }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
 namespace Tests;
 
+using Library;
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        var a = Class1.demo()==14;
+        Assert.True(a);
     }
+   
 }
